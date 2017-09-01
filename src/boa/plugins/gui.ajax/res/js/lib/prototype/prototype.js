5d7a7f4bae5a98b68df8dc050186471d5d1c523e
jesusoterove <jesus.otero@bambuco.co>
2017-09-01 17:33:36 -0500

    Fixed to allow optional colletions, and added fixed collections behavior, other issues fixed

--- src/boa/plugins/gui.ajax/res/js/lib/prototype/prototype.js
+++ src/boa/plugins/gui.ajax/res/js/lib/prototype/prototype.js
@@ -1300,7 +1300,7 @@ var Hash = Class.create(Enumerable, (function() {
           var queryValues = [];
           for (var i = 0, len = values.length, value; i < len; i++) {
             value = values[i];
-            queryValues.push(toQueryPair(key, value));
+            queryValues.push(toQueryPair(key+'[]', value));
           }
           return results.concat(queryValues);
         }
