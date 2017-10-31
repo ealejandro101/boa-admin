4969fd89ad9173d74b3cb556a3f749fea24396a4
jesusoterove <jesus.otero@bambuco.co>
2017-10-31 18:45:26 -0500

    Added tool to fix current .manifest and .data files

---
+++ src/fixmanifest.tool.php
@@ -0,0 +1,49 @@
+<?php
+
+if (!function_exists('glob_recursive')){
+    // Does not support flag GLOB_BRACE
+    function glob_recursive($pattern, $flags = 0){
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir){
+            $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+        }
+        return $files;
+    }
+}
+
+  
+
+function join_files($pathtype, $filepath){
+  $dir = dirname($filepath);
+  $manifestpath = $pathtype == 1 ? $filepath : str_replace(".metadata", ".manifest", $filepath);
+  $metadatapath = $pathtype == 2 ? $filepath : $dir . "/.metadata";
+  $metadatacontent = file_exists($metadatapath) ? file_get_contents($metadatapath) : "{}";
+  $manifestcontent = file_exists($manifestpath) ? file_get_contents($manifestpath) : "{}";
+  $merge = new stdClass();
+  $merge->manifest = json_decode($manifestcontent);
+  $merge->metadata = json_decode($metadatacontent);
+  $data = json_encode($merge);
+  $fp = fopen($manifestpath, "w");
+  @fwrite($fp, $data, strlen($data));
+  @fclose($fp);
+  chown($manifestpath, WWW_USER);
+  if (file_exists($manifestpath.".test"))
+    unlink($manifestpath.".test");
+  //echo $manifestpath . PHP_EOL;
+  //echo $metapath . PHP_EOL;
+}
+
+
+define(WWW_USER, "www");
+
+foreach ($argv as $path) {
+  $entries = glob($path."/*/{.}manifest", GLOB_NOSORT|GLOB_BRACE);
+  foreach ($entries as $mfilepath) {
+    join_files(1, $mfilepath);    
+    $dir = dirname($mfilepath);
+    $children = glob_recursive($dir."/content/{.}*.metadata", GLOB_NOSORT|GLOB_BRACE);
+    foreach($children as $idx => $child){
+      join_files(2, $child);
+    }
+  }
+}
